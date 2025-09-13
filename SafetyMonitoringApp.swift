98f0544a972d07c8990d4bfb413ab89fdb5cf460
Renat <113312541+Renarion@users.noreply.github.com>
2025-09-13 10:21:35 +0300

    Add files via upload

---
+++ SafetyMonitoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafetyMonitoringApp.swift
+//  SafetyMonitoring
+//
+//  Created by Aleksei Pleshkov on 11/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct SafetyMonitoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
